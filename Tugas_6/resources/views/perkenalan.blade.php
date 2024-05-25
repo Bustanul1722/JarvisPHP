b97c1a6b64db61066d21faf2464ff09a0b5f9fe9
Bustanul1387 <bustanularifin1722@gmail.com>
2024-05-25 12:09:23 +0700

    Tugas6

---
+++ Tugas_6/resources/views/perkenalan.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Perkenalan</title>
+
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                margin: 0;
+                padding: 0;
+                background-color: #F0F8FF;
+                color: #000000;
+            }
+            .kotak {
+                max-width: 500px;
+                margin: 100px auto;
+                padding: 20px;
+                background-color: #FFFFFF;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                border-radius: 8px;
+            }
+            h1 {
+                text-align: center;
+            }
+            .datadiri {
+                margin-bottom: 20px;
+            }
+            .datadiri p {
+                margin: 5px 0;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div class="kotak">
+            <h1>PERKENALAN</h1>
+            
+            <div class="datadiri">
+                <P>Halo semua, perkenalkan saya</p>
+                <p>Nama : Bustanul Arifin</p>
+                <p>Umur : 22 tahun</p>
+                <p>Hobi : Bermain game dan menonton film</p>
+                <p>Kesibukan : Kuliah</p>
+            </div>
+        </div>
+    </body>
+</html>
